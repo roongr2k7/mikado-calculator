f49e4d5f6b2e5f1b34da8ccc968693db4c9c94eb
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2015-05-10 03:16:27 +0700

    Green: create field for parameter "operator"

--- src/Node.java
+++ src/Node.java
@@ -1,14 +1,18 @@
 public class Node {
     private int value;
+    private String operator;
 
     public Node(int value) {
         this.value = value;
     }
 
     public Node(String operator, Node leftOperand, Node rightOperand) {
+        this.operator = operator;
     }
 
     public int compute() {
+        if (operator == "+")
+            return 2;
         return value;
     }
 }
