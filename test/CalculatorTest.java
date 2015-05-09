f64b2c941117358f579ab72fea54eef1d13aa845
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2015-05-10 02:29:19 +0700

    first failing test

---
+++ test/CalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void compute() {
+        assertEquals(1, 0);
+    }
+}
